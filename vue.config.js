d87cf2a6dc3e2f9597a8619af8a96a249eec2ab9
Janos Perge <janos.perge@gmail.com>
2024-01-10 00:54:37 +0100

    fix prod route

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-black-jack/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue2-black-jack/" : "/",
   pages: {
     index: {
       entry: "src/main.js",
