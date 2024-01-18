debb0c2a0ad17c670271b174c5ce76fdbd3b29a4
Janos Perge <janos.perge@gmail.com>
2024-01-18 21:59:03 +0100

    run build, extend config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  runtimeCompiler: true,
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/vue2-black-jack/" : "/",
   pages: {
