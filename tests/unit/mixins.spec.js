eaa540119e0eadf05d52549ebb0a0a3e38a61a2a
Janos Perge <janos.perge@gmail.com>
2024-01-19 20:58:45 +0100

    add calculateDeckValue and add calculateCardValue tests

--- tests/unit/mixins.spec.js
+++ tests/unit/mixins.spec.js
@@ -2,16 +2,50 @@ import { mount } from "@vue/test-utils";
 import mixins from "@/mixins";
 
 describe("mixins", () => {
-  it("should calculate card value correctly", () => {
-    const wrapper = mount({
-      template: "<div></div>",
-      mixins: [mixins],
+  describe("calculate card value:", () => {
+    test.each`
+      card     | value
+      ${"H10"} | ${"10"}
+      ${"H8"}  | ${"8"}
+      ${"H6"}  | ${"6"}
+      ${"H4"}  | ${"4"}
+      ${"H2"}  | ${"2"}
+      ${"S9"}  | ${"9"}
+      ${"S7"}  | ${"7"}
+      ${"S5"}  | ${"5"}
+      ${"S3"}  | ${"3"}
+      ${"SA"}  | ${"A"}
+      ${"HA"}  | ${"A"}
+      ${"DA"}  | ${"A"}
+      ${"CA"}  | ${"A"}
+    `("card is $card value will be: $value", ({ card, value }) => {
+      const wrapper = mount({
+        template: "<div></div>",
+        mixins: [mixins],
+      });
+
+      const actualResult = wrapper.vm.getCardValue(card);
+
+      expect(actualResult).toBe(value);
     });
+  });
 
-    // Call the getCardValue method with a test card
-    const cardValue = wrapper.vm.getCardValue("H10");
+  describe("calculate deck value", () => {
+    test.each`
+      deck                         | value
+      ${["H10", "D8"]}             | ${18}
+      ${["H10", "S2", "D8"]}       | ${20}
+      ${["D7", "H10", "DA", "S3"]} | ${21}
+      ${["H8", "D8", "S8", "C5"]}  | ${29}
+    `("deck contains $deck, so value will be $value", ({ deck, value }) => {
+      const wrapper = mount({
+        template: "<div></div>",
+        mixins: [mixins],
+      });
 
-    // Add your assertions here based on the expected behavior
-    expect(cardValue).toBe("10");
+      const actualResult = wrapper.vm.calcDeckValue(deck);
+
+      expect(actualResult).toBe(value);
+    });
   });
 });
